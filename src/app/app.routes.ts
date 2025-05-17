ca48cf2ebeffab60d44313ba81c75e621edf5c1e
DJ Oakman <djoakman512@gmail.com>
2025-05-17 17:31:59 -0500

    I like the state now

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,12 +2,18 @@ import { Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { MainLayoutComponent } from './main-layout/main-layout.component';
+import { AssignmentsComponent } from './assignments/assignments.component';
+import { AuthGuard } from './auth.guard';
 
 export const appRoutes: Routes = [
   { path: '', component: LoginComponent },
   {
     path: '',
     component: MainLayoutComponent,
-    children: [{ path: 'dashboard', component: DashboardComponent }],
+    canActivate: [AuthGuard],
+    children: [
+      { path: 'dashboard', component: DashboardComponent },
+      { path: 'assignments', component: AssignmentsComponent },
+    ],
   },
 ];
