1ed03262e1208109edfad61045f0f5b980598e11
Kate Moreland <kate.ella.moreland@gmail.com>
2025-09-18 13:08:10 -0500

    Updated environments

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,1 +1,1 @@
-export const environment = { production: false, apiUrl: 'http://my-node-env.eba-ysg2ei62.us-east-1.elasticbeanstalk.com/upload-pdf' };
+export const environment = { production: false, apiUrl: 'http://my-node-env.eba-ysg2ei62.us-east-1.elasticbeanstalk.com' };
