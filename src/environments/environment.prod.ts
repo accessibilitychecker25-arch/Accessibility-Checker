40d59873107978bf67e7651230669c743003a332
Kate Moreland <kate.ella.moreland@gmail.com>
2025-09-19 00:13:50 -0500

    Update build output for GitHub Pages

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,1 +1,1 @@
-export const environment = { production: true, apiUrl: 'https://my-node-env.eba-ysg2ei62.us-east-1.elasticbeanstalk.com' };
+export const environment = { production: true, apiUrl: 'https://my-node-env.eba-ysg2ei62.us-east-1.elasticbeanstalk.com/upload-pdf' };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,1 +1,1 @@
-export const environment = { production: false, apiUrl: 'https://my-node-env.eba-ysg2ei62.us-east-1.elasticbeanstalk.com' };
+export const environment = { production: false, apiUrl: 'https://my-node-env.eba-ysg2ei62.us-east-1.elasticbeanstalk.com/upload-pdf' };
