413c4475a0935b71da92067dcab61bbda8b8296a
Kate Moreland <kate.ella.moreland@gmail.com>
2025-09-20 23:19:55 -0500

    Fix build configuration and update backend URL

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = { 
   production: true, 
-  apiUrl: 'https://api.accessibilitychecker.app/upload-pdf' 
+  apiUrl: 'http://my-node-env.eba-ysg2ei62.us-east-1.elasticbeanstalk.com/upload-pdf' 
 };
